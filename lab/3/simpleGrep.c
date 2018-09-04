f61dc776a762fff38c8ed302153e4f00b9a0b898
Charlie <a01373264@itesm.mx>
2018-09-04 16:52:17 -0500

    Problemas 3 y 4

---
+++ lab/3/simpleGrep.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int simpleGrep(char *f, char *word, int caso)
+{
+    FILE *openFile;
+    int line =1, count=0;
+    char currLine[512];
+    if((openFile = fopen(f,"r"))==NULL)
+    {
+	printf("Empty file! \n");
+	return (0);
+    }
+    while(fgets(currLine, 512, openFile)!=NULL)
+    {
+        if(strstr(currLine,word)!=NULL)
+	{
+	    printf("\n %s \n",currLine);
+	    count++;
+	}
+	line++;
+    }
+    if(count==0)
+    {
+	printf("No match found! \n");
+    }
+    if(caso)
+    {
+	printf("Times the word was found: %d \n", count);
+    }
+    fclose(openFile);
+    return(0);
+}
+
+int main(int argc, char *argv[]){
+    if (argc ==3)
+    {
+        printf("The file to open is %s the word to search is %s\n", argv[1], argv[2]);
+        simpleGrep(argv[1], argv[2],0);
+    }
+    else if(argc == 4)
+    {
+        printf("The file to open is %s the word to search is %s and the action to do is %s\n ",argv[1], argv[2], argv[3]);
+        simpleGrep(argv[1], argv[2], 1);
+    }
+    else if(argc>3)
+    {
+        printf("Too many arguments supplied. \n");
+    }
+    else {
+        printf("Please enter a file and a word. \n");
+    }
+}
+
