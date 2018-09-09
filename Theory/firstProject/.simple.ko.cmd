756731cf5fe4904f43ac8faadafa3d97bbda2466
A01370139@itesm.mx <A01370139@itesm.mx>
2018-09-09 19:33:09 +0000

    compiled module

---
+++ Theory/firstProject/.simple.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/OS/Theory/firstProject/simple.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/OS/Theory/firstProject/simple.ko /home/OS/Theory/firstProject/simple.o /home/OS/Theory/firstProject/simple.mod.o ;  true
