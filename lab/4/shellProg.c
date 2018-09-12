2de24a14bb4bf8bd2cf2212ecc4912a4716865ea
Charlie <a01373264@itesm.mx>
2018-09-11 23:20:30 -0500

    correction program 5

--- lab/4/shellProg.c
+++ lab/4/shellProg.c
@@ -6,22 +6,15 @@ Rodirigo Cuevas
 Carlos Carbajal*/
 int main(int argc,char *argv[] )
 {
-  char echo[5];
-  strcpy(echo, "echo");
   if(argc!=3)
   {
     printf("Check the arguments given \n");
   }
   else
   {
-    if(strcmp(argv[1], echo)==0)
-    {
-      execlp("echo","echo", argv[2], NULL);
-    }
-    else
-    {
-      printf("Unknown command \n");
-    }
+    execlp(argv[1],argv[1], argv[2], NULL);
   }
 }
+
+
 
