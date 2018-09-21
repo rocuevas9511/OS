c0139711ed093fbf689e8f29ac028baa66fc0fc1
Carlos E. Carbajal Nogués <a01373264@itesm.mx>
2018-09-20 21:15:18 -0500

    Update program6_a01370135_a01373264.c

--- lab/5/program6_a01370135_a01373264.c
+++ lab/5/program6_a01370135_a01373264.c
@@ -4,8 +4,63 @@
   Carlos Carbajal
 */
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+/* mycopy requires the caller to insure instr is NULL-terminated
+and outstr has enough space to hold a copy of instr. */
+void mycopy(char *instr, char *outstr)
+{
+//write your code here
+}
 
+/* slen returns the length of the string instr. instr is NULL-terminated */
+int slen(char *instr)
+{
+//write your code here
+}
+
+int func3(char *str, char *w)
+{
+  int idx,i,j;
+  for (i=0; str[i]; i++)
+  {
+    if (str[i] == w[j])
+    {
+      if (!j) idx = i;
+      j++;
+    }
+    else
+      j = 0;
+      if (w[j] == '\0')
+      break;
+   }
+    if (w[j] != '\0')
+      return(-1);
+    else
+      return(idx);
+}
+
+void main()
+{
+  char name[80];
+  char quote[256];
+  char name_copy[80];
+  int id;
+  
+  strcpy(name,"Michael Daconta");
+  strcpy(quote,"The C language has brought forth a new era in computing");
+  printf("name is %s.\n",name);
+  
+  mycopy(name,name_copy);
+  printf("name_copy is %s.\n",name_copy);
+  printf("the length of :\n%s\n is %d.\n",quote,slen(quote));
+  printf("the length of :\n%s\n is %d.\n",name_copy,slen(name_copy));
+
+  id = func3(quote, "forth");
+  if (id != -1)
+    // print results accordingly
+  else
+    // print results accordingly
+}
 
