1bd76850182559064cd70e6f045e6dd7c1ac853d
Carlos E. Carbajal Nogués <a01373264@itesm.mx>
2018-11-06 21:25:12 -0600

    Create program2_a01370139_a01373264.c

---
+++ lab/9/program2_a01370139_a01373264.c
@@ -0,0 +1,25 @@
+/**********************************************
+Lab #: 9
+
+Team #: 11
+
+Name1: Rodrigo Cuevas
+ID1: A01370139
+Name2: Carlos Carbajal
+ID2: A01373264
+
+Program Title: Producer-consumer problem
+
+Brief Description: an implementation of the 
+producer-consumer problem with the code 
+previously given
+
+Compilation command:
+gcc -o proCon program2_a01370139_a01373264.c
+-lpthread -lrt
+
+Execution command:
+./proCon
+**********************************************/
+#include <stdio.h>
+#include <stdlib.h>
