4e8ec7f92b03c751c4893361e383bce266986a27
Carlos E. Carbajal Nogués <a01373264@itesm.mx>
2018-11-07 12:43:02 -0600

    Update program1_a0137139_a01373264.c

--- lab/10/program1_a0137139_a01373264.c
+++ lab/10/program1_a0137139_a01373264.c
@@ -1,1 +1,36 @@
-//hello world
+/**********************************************
+Lab #: 10
+
+Team #: 11
+
+Name1: Rodrigo Cuevas
+ID1: A01370139
+Name2: Carlos Carbajal
+ID2: A01373264
+
+Program Title: Scheduling algorithms
+
+Brief Description: implementation for the 
+following scheduling algorithms
+
+Compilation command:
+gcc -o lab10 program1_a01370139_a01373264.c
+
+Execution command:
+
+./lab10 -fcfs input01.txt -> executes 
+First-come, First-served algorithm
+
+./lab10 -sjf input01.txt -> executes Shortest
+Job First algorithm
+
+./lab10 -rr input01.txt -> executes Round
+Robin algorithm
+
+./lab10 -p input01.txt -> executes Priority
+scheduling algorithm
+**********************************************/
+
+//libraries to import
+#include <stdio.h>
+#include <stdlib.h>
